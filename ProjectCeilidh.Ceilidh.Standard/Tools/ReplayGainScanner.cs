3f477fc27257e2c1a3033d128840054d2d70583c
Olivia Trewin <zancoproductions@gmail.com>
2018-10-04 18:20:15 -0500

    Improve libEBUR128 bindings, adaptive ReplayGain
    Created public wrapper classes for libEBUR128
    Created a new ReplayGain filter that computes an exponential moving average for gain, to enable normalization on infinite streams

--- ProjectCeilidh.Ceilidh.Standard/Tools/ReplayGainScanner.cs
+++ ProjectCeilidh.Ceilidh.Standard/Tools/ReplayGainScanner.cs
@@ -6,26 +6,17 @@ using ProjectCeilidh.Ceilidh.Standard.Filter;
 
 namespace ProjectCeilidh.Ceilidh.Standard.Tools
 {
-    public static unsafe class ReplayGainScanner
+    public static class ReplayGainScanner
     {
-        private const double TARGET_LUFS = -18;
+        public const double TARGET_LUFS = -18;
         
-        public static Version LibEbur128Version
-        {
-            get
-            {
-                ebur128_get_version(out var major, out var minor, out var patch);
-                return new Version(major, minor, patch);
-            }
-        }
-
         public static bool IsAvailable
         {
             get
             {
                 try
                 {
-                    return LibEbur128Version.Major == 1;
+                    return EbuR128State.Version.Major == 1;
                 }
                 catch (DllNotFoundException)
                 {
@@ -37,116 +28,124 @@ namespace ProjectCeilidh.Ceilidh.Standard.Tools
         public static Decibel GetSuggestedGain(AudioStream stream, double targetLufs = TARGET_LUFS)
         {
             if (!IsAvailable) throw new Exception();
-            
-            var state = ebur128_init((uint) stream.Format.Channels, (uint) stream.Format.SampleRate, Modes.Global);
-            try
+
+            using (var state = new EbuR128State(stream.Format, EbuR128Modes.Global))
             {
                 var buf = new byte[stream.Format.BytesPerFrame * stream.Format.SampleRate];
                 int len;
-                fixed (byte* ptr = buf)
-                    while ((len = stream.Read(buf, 0, buf.Length)) > 0)
-                    {
-                        Error err;
-                        switch (stream.Format.DataFormat.BytesPerSample)
-                        {
-                            case 2:
-                                err = ebur128_add_frames_short(state, (short*) ptr, new IntPtr(len / stream.Format.BytesPerFrame));
-                                break;
-                            case 4 when stream.Format.DataFormat.NumberFormat == NumberFormat.FloatingPoint:
-                                err = ebur128_add_frames_float(state, (float*) ptr, new IntPtr(len / stream.Format.BytesPerFrame));
-                                break;
-                            case 4 when stream.Format.DataFormat.NumberFormat == NumberFormat.Signed:
-                                err = ebur128_add_frames_int(state, (int*) ptr, new IntPtr(len / stream.Format.BytesPerFrame));
-                                break;
-                            case 8 when stream.Format.DataFormat.NumberFormat == NumberFormat.FloatingPoint:
-                                err = ebur128_add_frames_double(state, (double*) ptr, new IntPtr(len / stream.Format.BytesPerFrame));
-                                break;
-                            default:
-                                throw new InvalidDataException();
-                        }
-                        
-                        if (err != Error.Success)
-                            throw new InvalidDataException();
-                    }
+                while ((len = stream.Read(buf, 0, buf.Length)) > 0)
+                    state.AddFrames(buf, 0, len);
 
-                if (ebur128_loudness_global(state, out var loudness) != Error.Success)
-                    throw new InvalidDataException();
-                
-                return new Decibel(targetLufs - loudness);
+                return new Decibel(targetLufs - state.GetLoudness());
             }
-            finally
+        }
+    }
+
+    [Flags]
+    public enum EbuR128Modes
+    {
+        Momentary = 1 << 0,
+        ShortTerm = (1 << 1) | Momentary,
+        Global = (1 << 2) | Momentary,
+        LoudnessRange = (1 << 3) | ShortTerm,
+        SamplePeak = (1 << 4) | Momentary,
+        TruePeak = (1 << 5) | Momentary | SamplePeak,
+        Histogram = 1 << 6
+    }
+    
+    internal unsafe class EbuR128State : IDisposable
+    {
+        public static Version Version
+        {
+            get
             {
-                ebur128_destroy(ref state);
+                ebur128_get_version(out var major, out var minor, out var patch);
+                return new Version(major, minor, patch);
             }
+        }
+        
+        private State* _state;
+        private readonly AudioFormat _format;
+
+        public EbuR128State(AudioFormat format, EbuR128Modes mode)
+        {
+            _state = ebur128_init((uint) format.Channels, (uint) format.SampleRate, mode);
+            _format = format;
+        }
+
+        public void AddFrames(byte[] data, int offset, int count)
+        {
+            fixed (byte* ptr = &data[offset])
+                switch (_format.DataFormat.BytesPerSample)
+                {
+                    case 2:
+                        ebur128_add_frames_short(_state, (short*) ptr, count / _format.BytesPerFrame);
+                        break;
+                    case 4 when _format.DataFormat.NumberFormat == NumberFormat.Signed:
+                        ebur128_add_frames_int(_state, (int*) ptr, count / _format.BytesPerFrame);
+                        break;
+                    case 4 when _format.DataFormat.NumberFormat == NumberFormat.FloatingPoint:
+                        ebur128_add_frames_float(_state, (float*) ptr, count / _format.BytesPerFrame);
+                        break;
+                    case 8 when _format.DataFormat.NumberFormat == NumberFormat.FloatingPoint:
+                        ebur128_add_frames_double(_state, (double*) ptr, count / _format.BytesPerFrame);
+                        break;
+                    default:
+                        throw new InvalidDataException();
+                }
+        }
+
+        public double GetLoudness()
+        {
+            double loudness;
+            switch (_state->Mode)
+            {
+                case EbuR128Modes.Momentary:
+                    ebur128_loudness_momentary(_state, out loudness);
+                    break;
+                case EbuR128Modes.ShortTerm:
+                    ebur128_loudness_shortterm(_state, out loudness);
+                    break;
+                case EbuR128Modes.Global:
+                    ebur128_loudness_global(_state, out loudness);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            return loudness;
+        }
+
+        public void Dispose()
+        {
+            if (_state == null) return;
+            
+            ebur128_destroy(ref _state);
         }
         
         #region Native
 
-        [DllImport("libebur128")]
-        private static extern void ebur128_get_version(out int major, out int minor, out int patch);
-
-        [DllImport("libebur128")]
-        private static extern State* ebur128_init(uint channels, ulong sampleRate, Modes mode);
-
-        [DllImport("libebur128")]
-        private static extern void ebur128_destroy(ref State* state);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_set_channel(State* state, uint channelNumber, Channel value);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_add_frames_short(State* state, short* src, IntPtr frames);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_add_frames_int(State* state, int* src, IntPtr frames);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_add_frames_float(State* state, float* src, IntPtr frames);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_add_frames_double(State* state, double* src, IntPtr frames);
-
-        [DllImport("libebur128")]
-        private static extern Error ebur128_loudness_global(State* state, out double loudness);
-
-        private enum Channel
+        [StructLayout(LayoutKind.Sequential)]
+        private readonly struct NativeInt
         {
-            Unused = 0,
-            Left = 1,
-            Mp030 = 1,
-            Right = 2,
-            Mm030 = 2,
-            Center = 3,
-            Mp000 = 3,
-            LeftSurround = 4,
-            Mp110 = 4,
-            RightSurround = 5,
-            Mm110 = 5,
-            DualMono,
-            MpSc,
-            MmSc,
-            Mp060,
-            Mm060,
-            Mp090,
-            Mp135,
-            Mm135,
-            Mp180,
-            Up000,
-            Up030,
-            Um045,
-            Up090,
-            Um090,
-            Up110,
-            Um110,
-            Up135,
-            Um135,
-            Up180,
-            Tp000,
-            Bp000,
-            Bp045,
-            Bm045
-        }
+            public readonly IntPtr Value;
 
+            public NativeInt(IntPtr value)
+            {
+                Value = value;
+            }
+            
+            public static implicit operator NativeInt(IntPtr value) => new NativeInt(value);
+            public static implicit operator NativeInt(int value) => new NativeInt(new IntPtr(value));
+            public static implicit operator NativeInt(long value) => new NativeInt(new IntPtr(value));
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        private struct State
+        {
+            public EbuR128Modes Mode;
+        }
+        
         private enum Error
         {
             Success = 0,
@@ -155,27 +154,36 @@ namespace ProjectCeilidh.Ceilidh.Standard.Tools
             InvalidChannelIndex,
             NoChange
         }
+        
+        [DllImport("libebur128")]
+        private static extern void ebur128_get_version(out int major, out int minor, out int patch);
 
-        [Flags]
-        private enum Modes
-        {
-            Momentary = 1 << 0,
-            ShortTerm = (1 << 1) | Momentary,
-            Global = (1 << 2) | Momentary,
-            LoudnessRange = (1 << 3) | ShortTerm,
-            SamplePeak = (1 << 4) | Momentary,
-            TruePeak = (1 << 5) | Momentary | SamplePeak,
-            Histogram = 1 << 6
-        }
+        [DllImport("libebur128")]
+        private static extern State* ebur128_init(uint channels, ulong sampleRate, EbuR128Modes ebuR128Mode);
 
-        [StructLayout(LayoutKind.Sequential)]
-        private struct State
-        {
-            public Modes Mode;
-            public uint Channels;
-            public ulong SampleRate;
-            private IntPtr _internal;
-        }
+        [DllImport("libebur128")]
+        private static extern void ebur128_destroy(ref State* state);
+
+        [DllImport("libebur128")]
+        private static extern Error ebur128_add_frames_short(State* state, short* src, NativeInt frames);
+
+        [DllImport("libebur128")]
+        private static extern Error ebur128_add_frames_int(State* state, int* src, NativeInt frames);
+
+        [DllImport("libebur128")]
+        private static extern Error ebur128_add_frames_float(State* state, float* src, NativeInt frames);
+
+        [DllImport("libebur128")]
+        private static extern Error ebur128_add_frames_double(State* state, double* src, NativeInt frames);
+
+        [DllImport("libebur128")]
+        private static extern Error ebur128_loudness_global(State* state, out double loudness);
+        
+        [DllImport("libebur128")]
+        private static extern Error ebur128_loudness_momentary(State* state, out double loudness);
+        
+        [DllImport("libebur128")]
+        private static extern Error ebur128_loudness_shortterm(State* state, out double loudness);
 
         #endregion
     }
