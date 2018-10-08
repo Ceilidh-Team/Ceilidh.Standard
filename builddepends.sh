692f1e8c44765aced78a902e65b524b1ac3186fd
Olivia Trewin <trewinolivia@gmail.com>
2018-10-07 21:01:29 -0500

    Remove weird path stuff

--- builddepends.sh
+++ builddepends.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y libportaudio2
 
 echo Installing development dependencies...
 sudo apt-get -y install libasound-dev autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev
-sudo apt-get -y install yasm nasm libmp3lame-dev
+sudo apt-get -y install yasm nasm
 
 echo Downloading libebur128...
 wget https://github.com/jiixyj/libebur128/archive/v1.2.4.tar.gz
@@ -30,8 +30,8 @@ cd ~/ffmpeg_sources
 wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar jxf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --enable-libmp3lame --enable-shared
-PATH="$HOME/bin:$PATH" make -s -j
+./configure --enable-shared
+make -s -j
 sudo make install
 hash -r
 cd $TRAVIS_BUILD_DIR
